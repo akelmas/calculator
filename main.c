a05e662736e78b9f27e8eee5a2b78698e07e4be8
elmas <elm4s@icloud.com>
2024-04-14 17:25:58 +0000

    Implement sum

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+int sum(int a, int b){
+	return a + b;
+}
+
 int main(int argc, char** argv){
-	printf("Calculator App");
+	int operand1 = 0;
+	int operand2 = 0;
+	char operation = ' ';
+	
+	scanf("%d %c %d", &operand1, &operation, &operand2);
+
+	switch(operation){
+		case '+':
+			printf("%d", sum(operand1, operand2));
+			break;
+		default:
+			printf("Invalid operation");
+	}
+
 	return 0;
 }
 
